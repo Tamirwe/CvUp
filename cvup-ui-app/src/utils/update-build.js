6eee266cfc4d5af4aa9b4f6c86e44a2c8ded20f8
Tamir Weiss <tamir.we@gmail.com>
2023-08-13 09:58:46 +0300

    ddddd

--- cvup-ui-app/src/utils/update-build.js
+++ cvup-ui-app/src/utils/update-build.js
@@ -1,24 +1,12 @@
 const fs = require("fs");
-const filePath = "./package.json";
 
-const packageJson = JSON.parse(fs.readFileSync(filePath).toString());
-packageJson.buildDate = new Date().getTime();
+const sw_file = "./src/forceUpdateCache.js";
 
-fs.writeFileSync(filePath, JSON.stringify(packageJson, null, 2));
+const buildDate = `console.log(${new Date().getTime()})`;
 
-const jsonData = {
-  buildDate: packageJson.buildDate,
-};
-
-const jsonContent = JSON.stringify(jsonData);
-
-fs.writeFile("./public/meta.json", jsonContent, "utf8", function (error) {
+fs.writeFile(sw_file, buildDate, "utf8", (error) => {
   if (error) {
-    console.log(
-      "An error occured while saving build date and time to meta.json"
-    );
-    return console.log(error);
+    return console.warn(error);
   }
-
-  console.log("Latest build date and time updated in meta.json file");
+  return console.warn("All right");
 });

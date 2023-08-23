521fe4ea82e06e12471c0a3d88cf8536450be893
Tamir Weiss <tamir.we@gmail.com>
2023-08-23 22:41:05 +0300

    ccc

--- cvup-ui-app/src/components/header/Header.tsx
+++ cvup-ui-app/src/components/header/Header.tsx
@@ -48,37 +48,41 @@ export const Header = observer(() => {
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
+    // <div
+    //   style={{
+    //     position: "sticky",
+    //     backgroundColor: "#f3f4f5",
+    //     height: "3rem",
+    //   }}
+    // >
     <div
+      data-role="header"
+      data-position="fixed"
       style={{
-        position: "relative",
-        backgroundColor: "#f3f4f5",
-        height: "3rem",
+        position: "sticky",
+        height: "2.8rem",
+        width: "100%",
+        zIndex: 1290,
+        backgroundColor: "#fff",
+        top: 0,
+        left: 0,
       }}
     >
       <div
         style={{
-          position: "absolute",
-          height: "3rem",
-          width: "100%",
-          zIndex: 1290,
-          backgroundColor: "#fff",
+          //backgroundColor: "rgb(249 252 255)",
+          //border: "1px solid #f3f3f3",
+          margin: "5px 10px",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          borderRadius: "25px",
         }}
       >
-        <div
-          style={{
-            //backgroundColor: "rgb(249 252 255)",
-            //border: "1px solid #f3f3f3",
-            margin: "5px 10px",
-            height: "100%",
-            display: "flex",
-            alignItems: "center",
-            borderRadius: "25px",
-          }}
-        >
-          <Grid container spacing={0}>
-            <Grid item xs={7} pl={1}>
-              <Stack direction="row" alignItems="center" spacing={1}>
-                {/* <IconButton
+        <Grid container spacing={0}>
+          <Grid item xs={7} pl={1}>
+            <Stack direction="row" alignItems="center" spacing={1}>
+              {/* <IconButton
                   size="medium"
                   onClick={(e) => {
                     authStore.logout();
@@ -117,23 +121,22 @@ export const Header = observer(() => {
                 >
                   <CiEdit />
                 </IconButton> */}
-                {isMobile && (
-                  <IconButton
-                    size="medium"
-                    onClick={() => {
-                      generalStore.rightDrawerOpen = false;
-                      generalStore.leftDrawerOpen =
-                        !generalStore.leftDrawerOpen;
-                    }}
-                  >
-                    <BsList />
-                  </IconButton>
-                )}
+              {isMobile && (
+                <IconButton
+                  size="medium"
+                  onClick={() => {
+                    generalStore.rightDrawerOpen = false;
+                    generalStore.leftDrawerOpen = !generalStore.leftDrawerOpen;
+                  }}
+                >
+                  <BsList />
+                </IconButton>
+              )}
 
-                <SettingsMenu />
-                {candsStore.candDisplay && location.pathname === "/cv" && (
-                  <>
-                    {/* <IconButton
+              <SettingsMenu />
+              {candsStore.candDisplay && location.pathname === "/cv" && (
+                <>
+                  {/* <IconButton
                       title="Email to candidate"
                       sx={{ fontSize: "1.56rem" }}
                       size="medium"
@@ -143,28 +146,28 @@ export const Header = observer(() => {
                     >
                       <MdOutlineMarkEmailRead />
                     </IconButton> */}
-                    <IconButton
-                      title="Review"
-                      sx={{ fontSize: "1.54rem", paddingTop: "0.4rem" }}
-                      size="small"
-                      onClick={() => {
-                        generalStore.showReviewCandDialog =
-                          !generalStore.showReviewCandDialog;
-                      }}
-                    >
-                      <CiEdit />
-                    </IconButton>
-                    <IconButton
-                      title="Email to customer"
-                      sx={{ fontSize: "1.54rem" }}
-                      size="small"
-                      onClick={() =>
-                        (generalStore.showEmailDialog = EmailTypeEnum.Contact)
-                      }
-                    >
-                      <MdOutlineMarkEmailUnread />
-                    </IconButton>
-                    {/*
+                  <IconButton
+                    title="Review"
+                    sx={{ fontSize: "1.54rem", paddingTop: "0.4rem" }}
+                    size="small"
+                    onClick={() => {
+                      generalStore.showReviewCandDialog =
+                        !generalStore.showReviewCandDialog;
+                    }}
+                  >
+                    <CiEdit />
+                  </IconButton>
+                  <IconButton
+                    title="Email to customer"
+                    sx={{ fontSize: "1.54rem" }}
+                    size="small"
+                    onClick={() =>
+                      (generalStore.showEmailDialog = EmailTypeEnum.Contact)
+                    }
+                  >
+                    <MdOutlineMarkEmailUnread />
+                  </IconButton>
+                  {/*
                     <Link
                       href={`${candsStore.downloadUrl}DD/GetFileStream?id=${candsStore.candDisplay.keyId}`}
                       download
@@ -178,77 +181,75 @@ export const Header = observer(() => {
                       </IconButton>
                     </Link> */}
 
-                    <IconButton
-                      title="Download original file"
-                      sx={{ fontSize: "1.54rem" }}
-                      size="small"
-                      onClick={async () => {
-                        const keyId = candsStore.candDisplay?.keyId;
+                  <IconButton
+                    title="Download original file"
+                    sx={{ fontSize: "1.54rem" }}
+                    size="small"
+                    onClick={async () => {
+                      const keyId = candsStore.candDisplay?.keyId;
 
-                        const data = await candsStore.getfile(keyId);
-                        if (!(data instanceof Blob)) return;
-                        const downloadedFile = new Blob([data!], {
-                          type: data.type,
-                        });
+                      const data = await candsStore.getfile(keyId);
+                      if (!(data instanceof Blob)) return;
+                      const downloadedFile = new Blob([data!], {
+                        type: data.type,
+                      });
 
-                        const a = document.createElement("a");
-                        a.setAttribute("style", "display:none;");
-                        document.body.appendChild(a);
-                        switch (data.type) {
-                          case "application/pdf":
-                            a.download = `${keyId}.pdf`;
-                            break;
-                          case "application/msword":
-                            a.download = `${keyId}.doc`;
-                            break;
-                          case "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
-                            a.download = `${keyId}.docx`;
-                            break;
-                          default:
-                            break;
-                        }
-                        a.href = URL.createObjectURL(downloadedFile);
-                        a.target = "_blank";
-                        a.click();
-                        document.body.removeChild(a);
-                      }}
-                    >
-                      <MdOutlineFileDownload />
-                    </IconButton>
-                    {candsStore.candDisplay &&
-                      positionsStore.candDisplayPosition && (
-                        <IconButton
-                          title="Customer review"
-                          sx={{ fontSize: "1.54rem", paddingTop: "0.4rem" }}
-                          size="small"
-                          onClick={() => {
-                            generalStore.showCustomerReviewCandDialog =
-                              !generalStore.showCustomerReviewCandDialog;
-                          }}
-                        >
-                          <MdOutlineEditNotifications />
-                        </IconButton>
-                      )}
-                  </>
-                )}
-              </Stack>
-            </Grid>
-            <Grid item xs={5} sx={{ textAlign: "right" }} pr={1}>
-              {isMobile && (
-                <IconButton
-                  size="medium"
-                  onClick={() => {
-                    generalStore.leftDrawerOpen = false;
-                    generalStore.rightDrawerOpen =
-                      !generalStore.rightDrawerOpen;
-                  }}
-                >
-                  <BsList />
-                </IconButton>
+                      const a = document.createElement("a");
+                      a.setAttribute("style", "display:none;");
+                      document.body.appendChild(a);
+                      switch (data.type) {
+                        case "application/pdf":
+                          a.download = `${keyId}.pdf`;
+                          break;
+                        case "application/msword":
+                          a.download = `${keyId}.doc`;
+                          break;
+                        case "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
+                          a.download = `${keyId}.docx`;
+                          break;
+                        default:
+                          break;
+                      }
+                      a.href = URL.createObjectURL(downloadedFile);
+                      a.target = "_blank";
+                      a.click();
+                      document.body.removeChild(a);
+                    }}
+                  >
+                    <MdOutlineFileDownload />
+                  </IconButton>
+                  {candsStore.candDisplay &&
+                    positionsStore.candDisplayPosition && (
+                      <IconButton
+                        title="Customer review"
+                        sx={{ fontSize: "1.54rem", paddingTop: "0.4rem" }}
+                        size="small"
+                        onClick={() => {
+                          generalStore.showCustomerReviewCandDialog =
+                            !generalStore.showCustomerReviewCandDialog;
+                        }}
+                      >
+                        <MdOutlineEditNotifications />
+                      </IconButton>
+                    )}
+                </>
               )}
-            </Grid>
+            </Stack>
           </Grid>
-        </div>
+          <Grid item xs={5} sx={{ textAlign: "right" }} pr={1}>
+            {isMobile && (
+              <IconButton
+                size="medium"
+                onClick={() => {
+                  generalStore.leftDrawerOpen = false;
+                  generalStore.rightDrawerOpen = !generalStore.rightDrawerOpen;
+                }}
+              >
+                <BsList />
+              </IconButton>
+            )}
+          </Grid>
+        </Grid>
       </div>
     </div>
   );

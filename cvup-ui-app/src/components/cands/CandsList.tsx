0259872dbd8f3690a61c9dad9e97e240c7b3e5f4
Tamir Weiss <tamir.we@gmail.com>
2023-08-29 21:40:58 +0300

    dddd

--- cvup-ui-app/src/components/cands/CandsList.tsx
+++ cvup-ui-app/src/components/cands/CandsList.tsx
@@ -104,8 +104,8 @@ export const CandsList = observer(
         sx={{
           height: isMobile
             ? advancedOpen
-              ? "calc(100vh - 208px)"
-              : "calc(100vh - 210px)"
+              ? "calc(100vh - 205px)"
+              : "calc(100vh - 156px)"
             : advancedOpen
             ? "calc(100vh - 173px)"
             : "calc(100vh - 125px)",

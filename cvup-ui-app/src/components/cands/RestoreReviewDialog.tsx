1c3cf0d404a7fe88f66fbf493284ee26e2e2d5cb
Tamir Weiss <tamir.we@gmail.com>
2025-04-01 22:31:14 +0300

    aaa

--- cvup-ui-app/src/components/cands/RestoreReviewDialog.tsx
+++ cvup-ui-app/src/components/cands/RestoreReviewDialog.tsx
@@ -18,19 +18,20 @@ export const RestoreReviewDialog = ({ isOpen, onClose }: IProps) => {
 
   useEffect(() => {
     const lastReviewCandDetails: IRestoreCandDetails = JSON.parse(
-      localStorage.getItem("LastReviewCandDetails") || ""
+      localStorage.getItem("LastReviewCandDetails") || '{"candidateId":0}'
     );
+
     setCand1(lastReviewCandDetails);
 
-    const lastReview = localStorage.getItem("LastReview") || "";
+    const lastReview = localStorage.getItem("LastReview") || "0";
     setReview1(lastReview);
 
     const prevReviewCandDetails: IRestoreCandDetails = JSON.parse(
-      localStorage.getItem("PrevReviewCandDetails") || ""
+      localStorage.getItem("PrevReviewCandDetails") || '{"candidateId":0}'
     );
     setCand2(prevReviewCandDetails);
 
-    const prevReview = localStorage.getItem("PrevReview") || "";
+    const prevReview = localStorage.getItem("PrevReview") || "0";
     setReview2(prevReview);
   }, []);
 
@@ -49,111 +50,115 @@ export const RestoreReviewDialog = ({ isOpen, onClose }: IProps) => {
         Restore Candidate Review
       </BootstrapDialogTitle>
       <DialogContent sx={{ overflow: "hidden", paddingBottom: "15px" }}>
-        <div
-          style={{
-            direction: "rtl",
-          }}
-        >
-          <div>
-            {review1 && (
-              <div>
-                {cand1?.firstName && (
+        {cand1?.candidateId! > 0 || cand2?.candidateId! > 0 ? (
+          <div
+            style={{
+              direction: "rtl",
+            }}
+          >
+            <div>
+              {review1 && (
+                <div>
+                  {cand1?.firstName && (
+                    <div
+                      style={{
+                        fontSize: "1rem",
+                        fontWeight: "bold",
+                        color: "gray",
+                      }}
+                    >{`${cand1?.firstName} ${cand1?.lastName}${
+                      cand1?.customerName ? " - " + cand1?.customerName : ""
+                    }${
+                      cand1?.positionName ? " - " + cand1?.positionName : ""
+                    }`}</div>
+                  )}
                   <div
                     style={{
-                      fontSize: "1rem",
-                      fontWeight: "bold",
-                      color: "gray",
+                      maxHeight: "20rem",
+                      overflow: "auto",
+                      padding: "5px",
                     }}
-                  >{`${cand1?.firstName} ${cand1?.lastName}${
-                    cand1?.customerName ? " - " + cand1?.customerName : ""
-                  }${
-                    cand1?.positionName ? " - " + cand1?.positionName : ""
-                  }`}</div>
-                )}
-                <div
-                  style={{
-                    maxHeight: "20rem",
-                    overflow: "auto",
-                    padding: "5px",
-                  }}
-                >
-                  <IconButton
-                    title="Copy Text"
-                    sx={{ fontSize: "1.54rem" }}
-                    size="small"
-                    onClick={() => navigator.clipboard.writeText(review1)}
                   >
-                    <CiUsb />
-                  </IconButton>
+                    <IconButton
+                      title="Copy Text"
+                      sx={{ fontSize: "1.54rem" }}
+                      size="small"
+                      onClick={() => navigator.clipboard.writeText(review1)}
+                    >
+                      <CiUsb />
+                    </IconButton>
 
-                  <pre
-                    style={{
-                      whiteSpace: "break-spaces",
-                      direction: "rtl",
-                      textAlign: "right",
-                      fontFamily: "inherit",
-                      margin: 0,
-                    }}
-                    dangerouslySetInnerHTML={{
-                      __html: review1 || "",
-                    }}
-                  ></pre>
+                    <pre
+                      style={{
+                        whiteSpace: "break-spaces",
+                        direction: "rtl",
+                        textAlign: "right",
+                        fontFamily: "inherit",
+                        margin: 0,
+                      }}
+                      dangerouslySetInnerHTML={{
+                        __html: review1 || "",
+                      }}
+                    ></pre>
+                  </div>
                 </div>
-              </div>
-            )}
-          </div>
-          <div style={{ padding: "2rem", textAlign: "center" }}>
-            ------------------------------------------------------
-          </div>
-          <div style={{ paddingBottom: "1rem" }}>
-            {review2 && (
-              <div>
-                {cand2?.firstName && (
+              )}
+            </div>
+            <div style={{ padding: "2rem", textAlign: "center" }}>
+              ------------------------------------------------------
+            </div>
+            <div style={{ paddingBottom: "1rem" }}>
+              {review2 && (
+                <div>
+                  {cand2?.firstName && (
+                    <div
+                      style={{
+                        fontSize: "1rem",
+                        fontWeight: "bold",
+                        color: "gray",
+                      }}
+                    >{`${cand2?.firstName} ${cand2?.lastName}${
+                      cand2?.customerName ? " - " + cand2?.customerName : ""
+                    }${
+                      cand2?.positionName ? " - " + cand2?.positionName : ""
+                    }`}</div>
+                  )}
                   <div
                     style={{
-                      fontSize: "1rem",
-                      fontWeight: "bold",
-                      color: "gray",
+                      maxHeight: "20rem",
+                      overflow: "auto",
+                      padding: "5px",
                     }}
-                  >{`${cand2?.firstName} ${cand2?.lastName}${
-                    cand2?.customerName ? " - " + cand2?.customerName : ""
-                  }${
-                    cand2?.positionName ? " - " + cand2?.positionName : ""
-                  }`}</div>
-                )}
-                <div
-                  style={{
-                    maxHeight: "20rem",
-                    overflow: "auto",
-                    padding: "5px",
-                  }}
-                >
-                  <IconButton
-                    title="Copy Text"
-                    sx={{ fontSize: "1.54rem" }}
-                    size="small"
-                    onClick={() => navigator.clipboard.writeText(review2)}
                   >
-                    <CiUsb />
-                  </IconButton>
+                    <IconButton
+                      title="Copy Text"
+                      sx={{ fontSize: "1.54rem" }}
+                      size="small"
+                      onClick={() => navigator.clipboard.writeText(review2)}
+                    >
+                      <CiUsb />
+                    </IconButton>
 
-                  <pre
-                    style={{
-                      whiteSpace: "break-spaces",
-                      direction: "rtl",
-                      textAlign: "right",
-                      fontFamily: "inherit",
-                      margin: 0,
-                    }}
-                    dangerouslySetInnerHTML={{
-                      __html: review2 || "",
-                    }}
-                  ></pre>
+                    <pre
+                      style={{
+                        whiteSpace: "break-spaces",
+                        direction: "rtl",
+                        textAlign: "right",
+                        fontFamily: "inherit",
+                        margin: 0,
+                      }}
+                      dangerouslySetInnerHTML={{
+                        __html: review2 || "",
+                      }}
+                    ></pre>
+                  </div>
                 </div>
-              </div>
-            )}
+              )}
+            </div>
           </div>
-        </div>
+        ) : (
+          <div style={{ fontSize: "2rem", padding: "4rem" }}>No data yet.</div>
+        )}
       </DialogContent>
     </Dialog>
   );

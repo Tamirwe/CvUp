f7d7fb118f6fb97cb46fd8ae775cecc67a6eb9b3
Tamir Weiss <tamir.we@gmail.com>
2026-06-03 14:57:59 +0300

    added search files

---
+++ CvUpSolution/CvAnalyzeEmbedOpenAiLibrary/ISearchCvsOpenAi.cs
@@ -0,0 +1,6 @@
+﻿namespace CvAnalyzeEmbedOpenAiLibrary
+{
+    public interface ISearchCvsOpenAi
+    {
+    }
+}
---
+++ CvUpSolution/PgVectorLibrary/SearchCvsService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PgVectorLibrary
+{
+    public class SearchCvsService: ISearchCvsService
+    {
+    }
+}
